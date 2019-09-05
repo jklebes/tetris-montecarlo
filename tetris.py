301f6c249608fc1fbc9fb8c4ef6fe2e749e996bd
Jason Klebes <jsklebes@googlemail.com>
2019-09-05 18:25:27 +0100

    start of initial attempt at structure

--- tetris.py
+++ tetris.py
@@ -1,2 +1,28 @@
+class Blocks(object):
+    pass
+
+class Grid(object):
+    def __init__(self, xsize, ysize):
+        self.xsize=xsize
+        self.ysize=ysize
+        self.occupancies = [[0]* self.xsize]* self.ysize
+
+    def step(self, temperature=0):
+        pass
+
+    def run(self, nsteps):
+        pass
+
+    def equilibrate(self):
+        pass
+
+
+
+
+
 if __name__ == "__main__":
     print("running")
+    testgrid = Grid(10,10)
+    print("created testgrid:", testgrid)
+
+
